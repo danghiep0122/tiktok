996ebbe99f94e5e15c092f5d423ce8701154eba5
D.Hiep <vostro1250@gmail.com>
2022-06-28 17:52:23 +0700

    Done Header#2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://www.logolynx.com/images/logolynx/e6/e6387f4999c065bde60e86ee7f8a0be3.png"
+                alt="abc"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Nguyen Van A</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
